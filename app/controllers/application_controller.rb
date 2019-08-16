2bcdfc459f8b474ef610bba56d106256da056db2
Trish Ogen <patriciaogen@gmail.com>
2019-08-16 02:38:56 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,13 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  # code actions here!
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb :index
+  end
+
+  get 'recipes/new' do
+    erb :new
+  end
 
 end
