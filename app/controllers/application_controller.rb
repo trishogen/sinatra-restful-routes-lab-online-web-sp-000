7bcb31bd9e74010cca995c4dfd9e4fe618d11718
Trish Ogen <patriciaogen@gmail.com>
2019-08-16 03:01:24 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -37,4 +37,10 @@ class ApplicationController < Sinatra::Base
 
     redirect to "/recipes/#{recipe.id}"
   end
+
+  delete '/recipes/:id' do
+    recipe = Recipe.find_by_id(params[:id]).destroy
+
+    redirect to '/recipes'
+  end
 end
